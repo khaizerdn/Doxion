72999db27850ae2e58bf85b61287bcd437ef2f1b
khaizerdn <khaizerdn@gmail.com>
2025-04-05 20:11:50 +0800

    Update Lockers.jsx

--- src/pages/Lockers.jsx
+++ src/pages/Lockers.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import BackButton from './components/BackButton';
 import Button from './components/Button';
 import Input from './components/Input';
