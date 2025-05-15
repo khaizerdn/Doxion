8f0f1dad1e0e4e6c25988a0bd5ff2c9cbf5bada8
khaizerdn <khaizerdn@gmail.com>
2025-05-15 21:03:49 +0800

    Added backspace

--- src/pages/components/GlobalKeyboardProvider.jsx
+++ src/pages/components/GlobalKeyboardProvider.jsx
@@ -148,7 +148,7 @@ const GlobalKeyboardProvider = () => {
           numbers: [
             '1 2 3 4 5 6 7 8 9 0',
             '- / : ; ( ) ₱ & @',
-            '. , ? ! \' " # % + =',
+            '. , ? ! \' " # % + = {bksp}',
             '{abc} {space} . {done}',
           ],
         }}
