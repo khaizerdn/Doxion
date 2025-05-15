475c2ebea822939c96cc0c63726ba76a993daa40
khaizerdn <khaizerdn@gmail.com>
2025-05-15 21:11:26 +0800

    Update GlobalKeyboardProvider.jsx

--- src/pages/components/GlobalKeyboardProvider.jsx
+++ src/pages/components/GlobalKeyboardProvider.jsx
@@ -134,12 +134,14 @@ const GlobalKeyboardProvider = () => {
         onKeyPress={onKeyPress}
         layout={{
           default: [
+            '1 2 3 4 5 6 7 8 9 0',
             'q w e r t y u i o p',
             'a s d f g h j k l',
             '{shift} z x c v b n m {bksp}',
             '{numbers} {space} . {done}',
           ],
           shift: [
+            '1 2 3 4 5 6 7 8 9 0',
             'Q W E R T Y U I O P',
             'A S D F G H J K L',
             '{shift} Z X C V B N M {bksp}',
